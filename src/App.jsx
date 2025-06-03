004f256f78bc90fc657cd74276365381c7ac920f
bhagwan <bhagwandas100710@gmail.com>
2025-06-03 12:11:16 +0530

    navbar problem removed

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Landing from './pages/Landing';
 import Home from './pages/Home';
 import Login from './pages/login';
 import Navbar from './components/Navbar';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
 
 function App() {
   const [user, setUser] = useState(null);
